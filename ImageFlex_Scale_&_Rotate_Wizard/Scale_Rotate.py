aa402577a0a7f45b95689ffebc99ea3fb7a91c56
AishwaryaDekhane <46292151+AishwaryaDekhane@users.noreply.github.com>
2024-01-13 17:37:51 -0500

    Add files via upload
    In OpenCV, scaling (`cv2.resize()`) adjusts image size, crucial for processing. Rotating (`cv2.getRotationMatrix2D` and `cv2.warpAffine()`) changes orientation for tasks like object alignment. Both operations are vital in image manipulation and computer vision applications.

---
+++ ImageFlex_Scale_&_Rotate_Wizard/Scale_Rotate.py
@@ -0,0 +1,27 @@
+#Author: Aishwarya Dekhane  
+#Date: 12 Jan, 2024
+
+import numpy as np
+import cv2
+
+#1 - colored image
+img = cv2.imread("/Users/aishwaryadekhane/Desktop/players.jpg",1)
+
+# Scale
+img_half = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
+img_stretch = cv2.resize(img, (600,600))
+
+#nearest interpolation 
+img_stretch_near = cv2.resize(img, (600,600), interpolation=cv2.INTER_NEAREST)
+
+cv2.imshow("Half",img_half)
+cv2.imshow("Stretch",img_stretch)
+cv2.imshow("Stretch near",img_stretch_near)
+
+# Rotation
+M = cv2.getRotationMatrix2D((0,0), -30, 1)
+rotated = cv2.warpAffine(img, M, (img.shape[1], img.shape[0]))
+cv2.imshow("Rotated",rotated)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
